73ffbed0d397356b68d97cf254d09dac16072ee6
mormis10 <cerdasignacio5@gmail.com>
2025-03-18 13:41:49 -0400

    Listo para pruebas en el lab

--- Trabajo_clase _01/Socket.cc
+++ Trabajo_clase _01/Socket.cc
@@ -1,136 +1,126 @@
 /**
-  *  Universidad de Costa Rica
-  *  ECCI
-  *  CI0123 Proyecto integrador de redes y sistemas operativos
-  *  2025-i
-  *  Grupos: 1 y 3
-  *
-  *  ******   Socket class implementation
-  *
-  * (Fedora version)
-  *
+ *  Universidad de Costa Rica
+ *  ECCI
+ *  CI0123 Proyecto integrador de redes y sistemas operativos
+ *  2025-i
+ *  Grupos: 1 y 3
+ *
+ *  ******   Socket class implementation
+ *
+ * (Fedora version)
+ *
  **/
 
-#include <sys/socket.h>         // sockaddr_in
-#include <arpa/inet.h>          // ntohs
-#include <unistd.h>		// write, read
+#include <arpa/inet.h> // ntohs
 #include <cstring>
-#include <stdexcept>   // esto es para la biblio de runtime error
-#include <stdio.h>		// printf
+#include <stdexcept>    // esto es para la biblio de runtime error
+#include <stdio.h>      // printf
+#include <sys/socket.h> // sockaddr_in
+#include <unistd.h>     // write, read
 
-#include "Socket.h"		// Derived class
-
-/**
-  *  Class constructor
-  *     use Unix socket system call
-  *
-  *  @param     char t: socket type to define
-  *     's' for stream
-  *     'd' for datagram
-  *  @param     bool ipv6: if we need a IPv6 socket
-  *
- **/
-Socket::Socket( char t, bool IPv6 ){
-
-   this->BuildSocket( t, IPv6 );      // Call base class constructor
-
-}
-
+#include "Socket.h" // Derived class
 
 /**
-  *  Class destructor
-  *
-  *  @param     int id: socket descriptor
-  *
+ *  Class constructor
+ *     use Unix socket system call
+ *
+ *  @param     char t: socket type to define
+ *     's' for stream
+ *     'd' for datagram
+ *  @param     bool ipv6: if we need a IPv6 socket
+ *
  **/
-Socket::~Socket() {
+Socket::Socket(char t, bool IPv6) {
 
+  this->BuildSocket(t, IPv6); // Call base class constructor
 }
-
 
 /**
-  * MakeConnection method
-  *   use "EstablishConnection" in base class
-  *
-  * @param      char * host: host address in dot notation, example "10.1.166.62"
-  * @param      int port: process address, example 80
-  *
+ *  Class destructor
+ *
+ *  @param     int id: socket descriptor
+ *
  **/
-int Socket::MakeConnection( const char * hostip, int port ) {
-
-   return this->EstablishConnection( hostip, port );
-
-}
-
+Socket::~Socket() {}
 
 /**
-  * MakeConnection method
-  *   use "EstablishConnection" in base class
-  *
-  * @param      char * host: host address in dns notation, example "os.ecci.ucr.ac.cr"
-  * @param      char * service: process address, example "http"
-  *
+ * MakeConnection method
+ *   use "EstablishConnection" in base class
+ *
+ * @param      char * host: host address in dot notation, example "10.1.166.62"
+ * @param      int port: process address, example 80
+ *
  **/
-int Socket::MakeConnection( const char *host, const char *service ) {
+int Socket::MakeConnection(const char *hostip, int port) {
 
-   return this->EstablishConnection( host, service );
-
+  return this->EstablishConnection(hostip, port);
 }
-
 
 /**
-  * Read method
-  *   use "read" Unix system call (man 3 read)
-  *
-  * @param      void * buffer: buffer to store data read from socket
-  * @param      int size: buffer capacity, read will stop if buffer is full
-  *
+ * MakeConnection method
+ *   use "EstablishConnection" in base class
+ *
+ * @param      char * host: host address in dns notation, example
+ *"os.ecci.ucr.ac.cr"
+ * @param      char * service: process address, example "http"
+ *
  **/
-size_t Socket::Read( void * buffer, size_t size ) {
-    // Aquí tenemos que utilizar la función de read
-    // para los parámetros ocupamos el id del socket-el buffer y el tamaño del buffer
-    ssize_t read_bytes = read(this->idSocket,buffer,size);
+int Socket::MakeConnection(const char *host, const char *service) {
 
-    if(read_bytes == -1){
-        throw std::runtime_error("Ocurrió un error obteniendo la cantidad de bytes que debemos de leer\n");
-    }
-
-    // Ahora toca castearlo para que vuelva a ser un entero sin signo 
-    static_cast<size_t>(read_bytes);
-
-   return read_bytes;
+  return this->EstablishConnection(host, service);
 }
-
 
 /**
-  * Write method
-  *   use "write" Unix system call (man 3 write)
-  *
-  * @param      void * buffer: buffer to store data write to socket
-  * @param      size_t size: buffer capacity, number of bytes to write
-  *
+ * Read method
+ *   use "read" Unix system call (man 3 read)
+ *
+ * @param      void * buffer: buffer to store data read from socket
+ * @param      int size: buffer capacity, read will stop if buffer is full
+ *
  **/
-size_t Socket::Write( const void * buffer, size_t size ) {
+size_t Socket::Read(void *buffer, size_t size) {
+  // Aquí tenemos que utilizar la función de read
+  // para los parámetros ocupamos el id del socket-el buffer y el tamaño del
+  // buffer
+  ssize_t read_bytes = read(this->idSocket, buffer, size);
 
-   ssize_t written_bytes = write(this->idSocket,buffer,size);
-   if(written_bytes == -1){
-     throw std::runtime_error("Ocurrió un error durante la lectura\n");
-   }
-// Aquí toca volver a hacer el casteo nacho
-   static_cast<size_t>(written_bytes);
-   return written_bytes;
+  if (read_bytes == -1) {
+    throw std::runtime_error("Ocurrió un error obteniendo la cantidad de bytes "
+                             "que debemos de leer\n");
+  }
 
+  // Ahora toca castearlo para que vuelva a ser un entero sin signo
+  static_cast<size_t>(read_bytes);
+
+  return read_bytes;
 }
-
 
 /**
-  * Write method
-  *   use "write" Unix system call (man 3 write)
-  *
-  * @param      char * text: text to write to socket
-  *
+ * Write method
+ *   use "write" Unix system call (man 3 write)
+ *
+ * @param      void * buffer: buffer to store data write to socket
+ * @param      size_t size: buffer capacity, number of bytes to write
+ *
  **/
-size_t Socket::Write( const char * text ) {
-   return Write(static_cast<const void *>(text), strlen(text));
+size_t Socket::Write(const void *buffer, size_t size) {
+
+  ssize_t written_bytes = write(this->idSocket, buffer, size);
+  if (written_bytes == -1) {
+    throw std::runtime_error("Ocurrió un error durante la lectura\n");
+  }
+  // Aquí toca volver a hacer el casteo nacho
+  static_cast<size_t>(written_bytes);
+  return written_bytes;
 }
 
+/**
+ * Write method
+ *   use "write" Unix system call (man 3 write)
+ *
+ * @param      char * text: text to write to socket
+ *
+ **/
+size_t Socket::Write(const char *text) {
+  return Write(static_cast<const void *>(text), strlen(text));
+}
--- Trabajo_clase _01/VSocket.cc
+++ Trabajo_clase _01/VSocket.cc
@@ -1,25 +1,25 @@
 /**
-  *  Universidad de Costa Rica
-  *  ECCI
-  *  CI0123 Proyecto integrador de redes y sistemas operativos
-  *  2025-i
-  *  Grupos: 1 y 3
-  *
-  ****** VSocket base class implementation
-  *
-  * (Fedora version)
-  *
+ *  Universidad de Costa Rica
+ *  ECCI
+ *  CI0123 Proyecto integrador de redes y sistemas operativos
+ *  2025-i
+ *  Grupos: 1 y 3
+ *
+ ****** VSocket base class implementation
+ *
+ * (Fedora version)
+ *
  **/
 
-#include <sys/socket.h>
-#include <arpa/inet.h>		// ntohs, htons
-#include <stdexcept>            // runtime_error
+#include <arpa/inet.h> // ntohs, htons
 #include <cerrno>
-#include <cstring>		// memset
 #include <cstdio>
-#include <netdb.h>			// getaddrinfo, freeaddrinfo
-#include <unistd.h>			// close
+#include <cstring> // memset
 #include <iostream>
+#include <netdb.h>   // getaddrinfo, freeaddrinfo
+#include <stdexcept> // runtime_error
+#include <sys/socket.h>
+#include <unistd.h> // close
 /*
 #include <cstddef>
 #include <cstdio>
@@ -28,201 +28,192 @@
 */
 #include "VSocket.h"
 
-
-
 /**
-  *  Class creator (constructor)
-  *     use Unix socket system call
-  *
-  *  @param     char t: socket type to define
-  *     's' for stream
-  *     'd' for datagram
-  *  @param     bool ipv6: if we need a IPv6 socket
-  *
+ *  Class creator (constructor)
+ *     use Unix socket system call
+ *
+ *  @param     char t: socket type to define
+ *     's' for stream
+ *     'd' for datagram
+ *  @param     bool ipv6: if we need a IPv6 socket
+ *
  **/
-void VSocket::BuildSocket( char t, bool IPv6 ){
-    this->IPv6 = IPv6;
-    this->type = t;
-    int domain;
-    int socket_type = SOCK_STREAM;
-    if(IPv6){
-      domain = AF_INET6;
-      return;
-    }
-    // tenemos que ver el tipo de dominio, cuando es de tipo IPv6 utilizamos el dominio AF_INET6 y en caso de IPv4 utilizamos AF_INET
-    domain = AF_INET;
-   // debería de utilizar más el operador terniario
-    
-    if(socket_type == -1){
-        throw std::runtime_error("VSocket::BuildSocket - Tipo de socket inválido");
-    }
+void VSocket::BuildSocket(char t, bool IPv6) {
+  this->IPv6 = IPv6;
+  this->type = t;
+  int domain;
+  int socket_type = SOCK_STREAM;
+  if (IPv6) {
+    domain = AF_INET6;
+    return;
+  }
+  // tenemos que ver el tipo de dominio, cuando es de tipo IPv6 utilizamos el
+  // dominio AF_INET6 y en caso de IPv4 utilizamos AF_INET
+  domain = AF_INET;
+  // debería de utilizar más el operador terniario
 
-    //Ahora utilizo la biblioteca para crear el socket
-    // para crear el socket y obtener su id ocupo el dominio y el tipo de socket
-    this->idSocket = socket(domain,socket_type,0);
+  if (socket_type == -1) {
+    throw std::runtime_error("VSocket::BuildSocket - Tipo de socket inválido");
+  }
 
-    if(this->idSocket == -1){
-        throw std::runtime_error("VSocket::BuildSocket - Error al crear el socket");
-    }else{
-        printf("Hecho con éxito nachito\n");
-    }
+  // Ahora utilizo la biblioteca para crear el socket
+  //  para crear el socket y obtener su id ocupo el dominio y el tipo de socket
+  this->idSocket = socket(domain, socket_type, 0);
 
+  if (this->idSocket == -1) {
+    throw std::runtime_error("VSocket::BuildSocket - Error al crear el socket");
+  } else {
+    printf("Hecho con éxito nachito\n");
+  }
 }
 
+/**
+ * Class destructor
+ *
+ **/
+VSocket::~VSocket() { this->Close(); }
 
 /**
-  * Class destructor
-  *
+ * Close method
+ *    use Unix close system call (once opened a socket is managed like a file in
+ *Unix)
+ *
  **/
-VSocket::~VSocket() {
-
-   this->Close();
-
-}
-
-
-/**
-  * Close method
-  *    use Unix close system call (once opened a socket is managed like a file in Unix)
-  *
- **/
-void VSocket::Close(){
-   if(this->idSocket != -1){
-    //busca el id dl socket y lo cierra
-    if(close(this->idSocket) == -1){
-       throw std::runtime_error("VSocket::Close()" );
+void VSocket::Close() {
+  if (this->idSocket != -1) {
+    // busca el id dl socket y lo cierra
+    if (close(this->idSocket) == -1) {
+      throw std::runtime_error("VSocket::Close()");
     }
     printf("Cierre realizado con éxito\n");
     this->idSocket = -1;
-   }
+  } else {
+    throw std::runtime_error("VSocket::Close()");
+  }
 }
 
-
 /**
-  * EstablishConnection method
-  *   use "connect" Unix system call
-  *
-  * @param      char * host: host address in dot notation, example "10.84.166.62"
-  * @param      int port: process address, example 80
-  *
+ * EstablishConnection method
+ *   use "connect" Unix system call
+ *
+ * @param      char * host: host address in dot notation, example "10.84.166.62"
+ * @param      int port: process address, example 80
+ *
  **/
-int VSocket::EstablishConnection( const char * hostip, int port ) {
-   int st = -1;
-   this->port = port;
-            struct sockaddr_in  host4;
-            memset( (char *) &host4, 0, sizeof( host4 ) );
-            host4.sin_family = AF_INET;
-            // Guarda aquí el id del host en binairo 
-            st = inet_pton( AF_INET, hostip, &host4.sin_addr );
-            if ( -1 == st ) {
-               throw( std::runtime_error( "VSocket::DoConnect, inet_pton" ));
-            }
-            //Asigna el número del puerto a la estrucutra
-            host4.sin_port = htons( port );
-            st = connect( this->idSocket, (sockaddr *) &host4, sizeof( host4 ) );
-            if ( -1 == st ) {
-                perror("Error en connect");
-               throw( std::runtime_error( "VSocket::DoConnect, connect" ));
-            }
+int VSocket::EstablishConnection(const char *hostip, int port) {
+  int st = -1;
+  this->port = port;
+  struct sockaddr_in host4;
+  memset((char *)&host4, 0, sizeof(host4));
+  host4.sin_family = AF_INET;
+  // Guarda aquí el id del host en binairo
+  st = inet_pton(AF_INET, hostip, &host4.sin_addr);
+  if (-1 == st) {
+    throw(std::runtime_error("VSocket::DoConnect, inet_pton"));
+  }
+  // Asigna el número del puerto a la estrucutra
+  host4.sin_port = htons(port);
+  st = connect(this->idSocket, (sockaddr *)&host4, sizeof(host4));
+  if (-1 == st) {
+    perror("Error en connect");
+    throw(std::runtime_error("VSocket::DoConnect, connect"));
+  }
 
-   if ( -1 == st ) {
-      throw std::runtime_error( "VSocket::EstablishConnection" );
-   }
+  if (-1 == st) {
+    throw std::runtime_error("VSocket::EstablishConnection");
+  }
 
-   return st;
-
+  return st;
 }
 
-int VSocket::EstablishConnection( const char * hostip, int port ,bool prove){
-  struct sockaddr_in6  host6;
-            struct sockaddr * ha;
-            int st;
-            memset( &host6, 0, sizeof( host6 ) );
-            host6.sin6_family = AF_INET6;
-            st = inet_pton( AF_INET6, hostip, &host6.sin6_addr );
-            if ( 0 <= st ) {	// 0 means invalid address, -1 means address error
-               throw std::runtime_error( "Socket::Connect( const char *, int ) [inet_pton]" );
-            }
-            host6.sin6_port = htons( port );
-            ha = (struct sockaddr *) &host6;
-            size_t len;
-            len = sizeof( host6 );
-            st = connect( this->idSocket, ha, len );
-            if ( -1 == st ) {
-               throw std::runtime_error( "Socket::Connect( const char *, int ) [connect]" );
-            }
+int VSocket::EstablishConnection(const char *hostip, int port, bool prove) {
+  struct sockaddr_in6 host6;
+  struct sockaddr *ha;
+  int st;
+  memset(&host6, 0, sizeof(host6));
+  host6.sin6_family = AF_INET6;
+  st = inet_pton(AF_INET6, hostip, &host6.sin6_addr);
+  if (0 <= st) { // 0 means invalid address, -1 means address error
+    throw std::runtime_error(
+        "Socket::Connect( const char *, int ) [inet_pton]");
+  }
+  host6.sin6_port = htons(port);
+  ha = (struct sockaddr *)&host6;
+  size_t len;
+  len = sizeof(host6);
+  st = connect(this->idSocket, ha, len);
+  if (-1 == st) {
+    throw std::runtime_error("Socket::Connect( const char *, int ) [connect]");
+  }
 }
 /**
   * EstablishConnection method
   *   use "connect" Unix system call
   *
-  * @param      char * host: host address in dns notation, example "os.ecci.ucr.ac.cr"
+  * @param      char * host: host address in dns notation, example
+"os.ecci.ucr.ac.cr"
   * @param      char * service: process address, example "http"
   *
- 
+
 **/
-int VSocket::EstablishConnection( const char *host, const char *service ) {
-   int st = -1;
-   struct  addrinfo hints, *res, *p;
-   // para esto tenemos que con hints es la estrucutra par definir el tipo de conexión que se desea 
-   //res va a tener la lista de direcciones y con p vamos a recorrer las direcciones obtenidas 
-   
-   // esto ya lo sabemos solo estamos añadiendo ceros dentro de hints para limpiarlo en caso de que hubiera basura
-   memset(&hints,0,sizeof(struct addrinfo));
-  
-  // Asignamos AF_INET6 para indicarle que deseamos solamente conexiones de tipo Ipv6
-   hints.ai_family = AF_INET6;
-   hints.ai_socktype = SOCK_STREAM;
-   hints.ai_flags = 0;
-   hints.ai_protocol = 0;
-   //Aquí específicamos que queremos un socket TCP
+int VSocket::EstablishConnection(const char *host, const char *service) {
+  int st = -1;
+  struct addrinfo hints, *res, *p;
+  // para esto tenemos que con hints es la estrucutra par definir el tipo de
+  // conexión que se desea
+  // res va a tener la lista de direcciones y con p vamos a recorrer las
+  // direcciones obtenidas
 
-   //busvamos la dirección del servidor
-   int status;
-   status = getaddrinfo(host,"8080",&hints,&res);
-   if(status != 0){
-     std::cerr << "Error en getaddrinfo: " << gai_strerror(status) << std::endl;
-   }
+  // esto ya lo sabemos solo estamos añadiendo ceros dentro de hints para
+  // limpiarlo en caso de que hubiera basura
+  memset(&hints, 0, sizeof(struct addrinfo));
 
-  
+  // Asignamos AF_INET6 para indicarle que deseamos solamente conexiones de tipo
+  // Ipv6
+  hints.ai_family = AF_INET6;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = 0;
+  hints.ai_protocol = 0;
+  // Aquí específicamos que queremos un socket TCP
 
-   //Ahora sí vamos a recorrer el arrego de direcciones que tenemos
-   // lo recorremos cómo sí fuera una lista enlazada
+  // busvamos la dirección del servidor
+  int status;
+  status = getaddrinfo(host, "8080", &hints, &res);
+  if (status != 0) {
+    std::cerr << "Error en getaddrinfo: " << gai_strerror(status) << std::endl;
+  }
 
-   
-   
-   for(p = res; p != nullptr; p = p->ai_next){
-    //creamos el socket
-    // obtenemos el id del socket
-    std::cout<<p->ai_protocol <<"\n";
-    this->idSocket = socket(p->ai_family,p->ai_socktype,p->ai_protocol);
+  // Ahora sí vamos a recorrer el arrego de direcciones que tenemos
+  //  lo recorremos cómo sí fuera una lista enlazada
+
+  for (p = res; p != nullptr; p = p->ai_next) {
+    // creamos el socket
+    //  obtenemos el id del socket
+    std::cout << p->ai_protocol << "\n";
+    this->idSocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
     // si la conexión falla, continue con la siguiente
-     if(this->idSocket == -1){
+    if (this->idSocket == -1) {
       continue;
-     }
-     // en caso de que sí fuera exitoso
-     // en ese caso sí nos conectamos 
+    }
+    // en caso de que sí fuera exitoso
+    // en ese caso sí nos conectamos
 
-     //std::cout<<p->ai_addr;
-     st = connect(this->idSocket,p->ai_addr,p->ai_addrlen);
-     // en caso de que la conexión se haya realizado de manera correcta 
-     // dejamos de recorrer el arreglo 
-     if(st == 0){
-       break;
-     }
-     // en caso de que algo haya pasado mal, entonces cerramos el socket
-     close(this->idSocket);
-   }
-   // liberamos la memoria dinámica alojada en res
-    freeaddrinfo(res);
+    // std::cout<<p->ai_addr;
+    st = connect(this->idSocket, p->ai_addr, p->ai_addrlen);
+    // en caso de que la conexión se haya realizado de manera correcta
+    // dejamos de recorrer el arreglo
+    if (st == 0) {
+      break;
+    }
+    // en caso de que algo haya pasado mal, entonces cerramos el socket
+    close(this->idSocket);
+  }
+  // liberamos la memoria dinámica alojada en res
+  freeaddrinfo(res);
 
-   
-   if(st == -1){
-   throw std::runtime_error( "VSocket::EstablishConnection" );
-   }
+  if (st == -1) {
+    throw std::runtime_error("VSocket::EstablishConnection");
+  }
 
-
-   return st;
-
+  return st;
 }
-
