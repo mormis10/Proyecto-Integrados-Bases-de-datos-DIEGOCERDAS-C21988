0c16910d5c1ebbece481639132e7ba348855c50e
mormis10 <cerdasignacio5@gmail.com>
2025-03-26 00:31:24 -0400

    Prueba

---
+++ Tarea_corta_03/SSLSocket.h
@@ -0,0 +1,49 @@
+
+/**
+  *  Universidad de Costa Rica
+  *  ECCI
+  *  CI0123 Proyecto integrador de redes y sistemas operativos
+  *  2025-i
+  *  Grupos: 1 y 3
+  *
+  *   SSL Socket class interface
+  *
+  * (Fedora version)
+  *
+ **/
+
+#ifndef SSLSocket_h
+#define SSLSocket_h
+
+#include "VSocket.h"
+#include <unistd.h>
+
+
+class SSLSocket : public VSocket{
+
+   public:
+      SSLSocket( char* hostip,int port, bool IPv6 = false );				// Not possible to create with UDP, client constructor
+      SSLSocket( char *, char *, bool = false );		// For server connections
+      SSLSocket( int );
+      SSLSocket(char*,char*,bool);
+      ~SSLSocket();
+      int Connect( const char *, int );
+      int Connect( const char *, const char * );
+      void ShowCerts();
+      const char * GetCipher();
+      size_t Read( void *, size_t );
+      size_t Write( const void *, size_t );
+      size_t Write( const char * );
+   private:
+      void Init( bool = false );		// Defaults to create a client context, true if server context needed
+      void InitContext( bool );
+      void LoadCertificates( const char *, const char * );
+
+// Instance variables      
+      void * SSLContext;				// SSL context
+      void * SSLStruct;					// SSL BIO (Basic Input/Output)
+
+};
+
+#endif
+
---
+++ Tarea_corta_03/VSocket.h
@@ -0,0 +1,42 @@
+
+
+/**
+  *  Universidad de Costa Rica
+  *  ECCI
+  *  CI0123 Proyecto integrador de redes y sistemas operativos
+  *  2025-i
+  *  Grupos: 1 y 3
+  *
+  ****** VSocket base class interface
+  *
+  * (Fedora version)
+  *
+ **/
+
+#ifndef VSocket_h
+#define VSocket_h
+ 
+class VSocket {
+   public:
+       void BuildSocket( char, bool = false );
+      ~VSocket();
+
+      void Close();
+      int EstablishConnection( const char *, int );
+      int EstablishConnection( const char *, const char * ,bool);
+      int EstablishConnection( const char *, int ,bool);
+      int Bind( int );
+      size_t sendTo( const void *, size_t, void * );
+      size_t recvFrom( void *, size_t, void * );
+      
+    virtual size_t Write(const void* buffer, size_t size) = 0;
+    virtual size_t Read(void* buffer, size_t size) = 0;
+   protected:
+      int idSocket;   // Socket identifier
+      bool IPv6;      // Is IPv6 socket?
+      int port;       // Socket associated port
+      char type;      // Socket type (datagram, stream, etc.)
+        
+};
+
+#endif // VSocket_h
