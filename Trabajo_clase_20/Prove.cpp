94afc00a1912a3707bd3f086f746b36577a06090
mormis10 <cerdasignacio5@gmail.com>
2025-06-14 19:25:42 -0400

    Solución de conflictos entrega pasada y adaptación al protocolo

--- Trabajo_clase_20/Prove.cpp
+++ Trabajo_clase_20/Prove.cpp
@@ -9,37 +9,117 @@
 #include <iostream>
 
 
-void server_connection_prove(char* art[], int* sockets){
+void server_connection_prove(char* art[], int* sockets,char* ips[]){
     int id;
     int ports[6] = {8001,8002,8003,8004,8005,8006};
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len;
+    for(int i = 0; i<6; i++){
+    int udp_id = socket(AF_INET, SOCK_DGRAM, 0);
+if (udp_id < 0) {
+    perror("Error al crear el socket UDP");
+    return;
+}
+
+struct sockaddr_in udp_mess;
+memset(&udp_mess, 0, sizeof(udp_mess)); 
+
+udp_mess.sin_port = htons(ports[i]);         
+udp_mess.sin_family = AF_INET;
+
+if (inet_pton(AF_INET, "127.0.0.1", &udp_mess.sin_addr) <= 0) {
+    perror("Error en inet_pton");
+    close(udp_id);
+    return;
+}
+
+// Mensaje a enviar (sin el null terminator, ya que es UDP)
+const char *request_udp = "GET /servers";
+ssize_t bytes_sent = sendto(udp_id, request_udp, strlen(request_udp), 0, 
+                           (struct sockaddr*)&udp_mess, sizeof(udp_mess));
+
+if (bytes_sent < 0) {
+    perror("Error en sendto");
+    close(udp_id);
+    return;
+}
+
+//close(udp_id);
+    }
+    //Vamos a hacer unas pruebas
+int udp_recieve = socket(AF_INET, SOCK_DGRAM, 0);
+  struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(8080);
+    
+    if (bind(udp_recieve, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error en bind");
+        close(udp_recieve);
+        return;
+    }
+for(int i = 0; i<6; i++){
+ struct sockaddr_in other;
+ socklen_t addr_len = sizeof(other);
+ char data[200] = {0};
+
+ //Lo pongo a hacer bind
+
+ ssize_t st = recvfrom(udp_recieve, data, 200, 0, 
+                         (struct sockaddr*)&other, &addr_len);
+                        
+data[st] = '\0';
+std::string pivot = std::string(data);
+bool ip_cond = true;
+std::string ip_pivot = "";
+std::string name_pivot = "";
+for(int i = 0; i<pivot.size(); i++){
+    if(pivot[i] == ' '){
+        ip_cond = false;
+    }else{
+        if(ip_cond){
+         ip_pivot+= pivot[i];
+        }else{
+            name_pivot+= pivot[i];
+        }
+    }
+}
+ips[i] = strdup(ip_pivot.c_str());
+art[i] = strdup(name_pivot.c_str());
+std::cout<<"Recibí la siguiente información\n";
+std::cout<<ips[i] <<"\n";
+std::cout<<art[i] <<"\n";
+}
+    struct sockaddr_in server_addr_tcp;
     id = socket(AF_INET,SOCK_STREAM,0);
     //Simulamos un brodcast donde hacemos connect con todas las islas
     char* solicitud = "GET/FIGURES";
     for(int i = 0; i<6; i++){
-     id = socket(AF_INET,SOCK_STREAM,0);
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(ports[i]);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    id = socket(AF_INET,SOCK_STREAM,0);
+    memset(&server_addr_tcp, 0, sizeof(server_addr_tcp));
+    server_addr_tcp.sin_family = AF_INET;
+    server_addr_tcp.sin_addr.s_addr = INADDR_ANY;
+    server_addr_tcp.sin_port = htons(ports[i]);
+    server_addr_tcp.sin_addr.s_addr = inet_addr("127.0.0.1");
      // Nos conectamos al servidor remoto
-    if (connect(id, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+     usleep(500000);
+    if (connect(id, (struct sockaddr*)&server_addr_tcp, sizeof(server_addr_tcp)) < 0) {
         perror("Error al conectar con el servidor remoto");
         close(id);
           // Liberamos la memoria
         return;
     }
+    sockets[i] = id;
     
     // Enviamos la solicitud de imagen al servidor remoto
+    // yo en realidad solo tengo que conectarme de hecho de momento
+    /*
     write(id, solicitud, strlen(solicitud));
-    sockets[i] = id;
     char* buffer = new char[200];
     int bytes_read = read(id,buffer,200);
     buffer[bytes_read] = '\0';
     art[i] = strdup(buffer);
     delete[] buffer;
+    */
     }
 
 }
@@ -48,6 +128,7 @@ void user_menu(char* art[], int* sockets){
     bool menu = true;
     int option = 0;
     char image_option[100];
+    image_option[99] = '\0';
     int flag = 0;
     while(menu){
         flag = 0;
@@ -56,6 +137,7 @@ void user_menu(char* art[], int* sockets){
             std::cout<<"Imágenes disponibles\n";
             std::cout<<art[i] <<"\n";
         }
+        
         std::cout<<"Ingrese 1 si desea obtener la imagen o 2 para salir del menú\n";
         std::cin>>option;
         switch(option){
@@ -91,7 +173,8 @@ void user_menu(char* art[], int* sockets){
 int main(){
     char* figures_name[6];
     int servers_sockets[6];
-    server_connection_prove(figures_name,servers_sockets);
+    char* ip[6];
+    server_connection_prove(figures_name,servers_sockets,ip);
     user_menu(figures_name,servers_sockets);
     for(int i = 0; i<6; i++){
         close(servers_sockets[i]);
--- Trabajo_clase_20/Servers.cpp
+++ Trabajo_clase_20/Servers.cpp
@@ -21,18 +21,80 @@ pthread_mutex_t *shared_mutex;
 }thread_data;
 
 void *start_servers_connection(void* data){
+    //vamo a recibir
     thread_data* tdata = (thread_data*)data;
-    int sockid, client_sockfd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len;
-    sockid = socket(AF_INET,SOCK_STREAM,0);
+    int sock_udp = socket(AF_INET,SOCK_DGRAM,0);
+    struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(tdata->my_port);
+    
+    if (bind(sock_udp, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error en bind");
+        close(sock_udp);
+        return nullptr;
+    }
+    struct sockaddr_in other;
+    socklen_t addr_len = sizeof(other);
+    char buffer_udp[201];
+    ssize_t st = recvfrom(sock_udp, buffer_udp, 200, 0, 
+                         (struct sockaddr*)&other, &addr_len);
+    if (st < 0) {
+        perror("Error en recvfrom");
+        close(sock_udp);
+        return nullptr;
+    };
+    buffer_udp[st] = '\0';
+    pthread_mutex_lock(tdata->shared_mutex);
+    std::cout<<"Mensaje recibido: " << buffer_udp <<"\n";
+    pthread_mutex_unlock(tdata->shared_mutex);
+   int sock_udp_send = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock_udp_send < 0) {
+        perror("Error al crear socket de envío");
+        close(sock_udp);
+        return nullptr;
+    }
+
+    struct sockaddr_in udp_mess;
+    memset(&udp_mess, 0, sizeof(udp_mess)); 
+    udp_mess.sin_port = htons(8080);         
+    udp_mess.sin_family = AF_INET;
+    
+    if (inet_pton(AF_INET, "127.0.0.1", &udp_mess.sin_addr) <= 0) {
+        perror("Error en inet_pton");
+        close(sock_udp);
+        close(sock_udp_send);
+        return nullptr;
+    }
+    //usleep(50000000);
+    const char *ip = "127.0.0.1";  // 9 bytes + null terminator
+    const char* figureN = tdata->figure_name;
+    std::string result = std::string(ip) + std::string(" ") + std::string(figureN);
+    const char* message = result.c_str();
+    ssize_t sent_bytes = sendto(sock_udp_send, message, strlen(message), 0, 
+                               (const struct sockaddr *)&udp_mess, 
+                               sizeof(udp_mess));
+    
+    if (sent_bytes < 0) {
+        perror("Error en sendto");
+    } else {
+        pthread_mutex_lock(tdata->shared_mutex);
+        std::cout << "Enviados " << sent_bytes << " bytes a 127.0.0.1:8080\n";
+        pthread_mutex_unlock(tdata->shared_mutex);
+    }    
+    //Aquí inicia la parte tcp, hay que hacer la parte TCP
+    int sockid, client_sockfd;
+    struct sockaddr_in server_addr_tcp, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    sockid = socket(AF_INET,SOCK_STREAM,0);
+    memset(&server_addr_tcp, 0, sizeof(server_addr_tcp));
+    server_addr_tcp.sin_family = AF_INET;
+    server_addr_tcp.sin_addr.s_addr = INADDR_ANY;
+    server_addr_tcp.sin_port = htons(tdata->my_port);
 
     // Enlazar el socket con la dirección
-    if (bind(sockid, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(sockid, (struct sockaddr*)&server_addr_tcp, sizeof(server_addr_tcp)) < 0) {
         perror("Error al enlazar socket");
         exit(1);
     }
@@ -50,8 +112,15 @@ void *start_servers_connection(void* data){
         if (client_id < 0) {
             perror("Error al aceptar conexión");
         }
-    char buffer[200];
+
+     pthread_mutex_lock(tdata->shared_mutex);
+    std::cout<<"El hilo número: " <<tdata->island_id <<" acepta la conexión\n";
+    pthread_mutex_unlock(tdata->shared_mutex);
+
+
+      /*char buffer[200];
       int bytes_read = read(client_id,buffer,200);
+      //int bytes_read = recvfrom(client_id, buffer, size, 0, (struct sockaddr *)addr, &addr_len);
     buffer[bytes_read] = '\0';
     pthread_mutex_lock(tdata->shared_mutex);
     std::cout<<"El server de la isal número: " <<tdata->island_id << "\n";
@@ -67,10 +136,13 @@ void *start_servers_connection(void* data){
     std::cout<<image <<"\n";
     pthread_mutex_unlock(tdata->shared_mutex);
     write(client_id,image,strlen(image));
-    char buffer2[200];
+    */
+    std::cout<<"llega aquí: " << tdata->island_id <<"\n";
+    char buffer2[200] = {0};
      int bytes_read2 = read(client_id,buffer2,200);
      buffer2[bytes_read2] = '\0';
-    pthread_mutex_lock(tdata->shared_mutex);
+     printf("Recibo esta solicitud %s \n",buffer2);
+    //pthread_mutex_lock(tdata->shared_mutex);
     const char* image2 = "   ******       ******\n"
 "  **      **   **      **\n"
 " **         ** **         **\n"
@@ -86,10 +158,10 @@ void *start_servers_connection(void* data){
 "           **   **\n"
 "             **\n";
 "";
-    // Pase lo que pase va a enviar este diseño por la prueba
     write(client_id,image2,strlen(image2));
-    pthread_mutex_unlock(tdata->shared_mutex);
-   // Ahora aquí tengo que enviar la data
+    //pthread_mutex_unlock(tdata->shared_mutex);
+     close(sock_udp);
+    close(sock_udp_send);
 }
 
 
